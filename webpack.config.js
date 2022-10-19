7bb9ccb02aad4e10664232ebd8698db0025489d0
Aminu Rabiu <arabiu033@gmail.com>
2022-10-19 09:29:18 +0100

    Run npm run bundle to update the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,42 @@
+/* eslint-disable */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+ module.exports = {
+   entry: {
+     index: './src/index.js',
+   },
+   mode: 'development',
+   devtool: 'inline-source-map',
+   devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, './dist'),
+     clean: true,
+   },
+   optimization: {
+    runtimeChunk: 'single',
   },
-  module: {
+   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
-  devServer: {
-    watchFiles: ['src/*'],
-    hot: true,
-    // static: './dist',
+  experiments: {
+    topLevelAwait: true
   },
-};
+ };
