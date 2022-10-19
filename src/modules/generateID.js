f64bf0af8c0cd3a7c38386f19cae8c3af15c6af8
Aminu Rabiu <arabiu033@gmail.com>
2022-10-19 15:21:35 +0100

    This file generate the API ID

---
+++ src/modules/generateID.js
@@ -0,0 +1,6 @@
+const promise = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps',{
+  method: 'POST'
+});
+const id = await promise.text();
+
+export default id;
