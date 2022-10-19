ba44f59f88cf211d00715f5ee9b2e77b76dcffd2
RichardChileya <richardchileyawk@gmail.com>
2022-10-19 13:05:46 +0200

    Add Pop up functionality

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import countries from './modules/countries.js';
 const ul = document.querySelector('ul');
 
 console.log(countries);
+// eslint-disable-next-line no-plusplus
 for (let i = 30; i < 42; i++) {
   const htmlText = `
   <li>
@@ -25,19 +26,92 @@ for (let i = 30; i < 42; i++) {
     </section>
     <button type="button" class="comment-btn">Comments</button>
   </li>
-  `
+  `;
   ul.insertAdjacentHTML('beforeend', htmlText);
 }
 
 document.getElementById('logo').src = logoImage;
-document.getElementById('search').style.backgroundImage =  `url(${icon})`;
+document.getElementById('search').style.backgroundImage = `url(${icon})`;
 
-document.querySelectorAll('.like').forEach(element => {
+document.querySelectorAll('.like').forEach((element) => {
   element.src = likeImage;
 });
 
-console.log(countries[38]);
+// console.log(countries[38]);
 
 document.querySelectorAll('.img-holder').forEach((val, index) => {
-  val.style.backgroundImage = `url(${countries[index + 88].flags.png})`
-})
+  val.style.backgroundImage = `url(${countries[index + 88].flags.png})`;
+});
+
+/* Pop Up  */
+const section = document.querySelector('#popup-section');
+const windowContainer = document.createElement('div');
+windowContainer.className = 'popup-window';
+windowContainer.innerHTML = ` 
+
+  <i class="fa-solid fa-x">X</i>
+
+ 
+<div class = 'side-bars'>
+
+  <img class="popUp-img" src="./images/geek.webp" alt="">
+  <h2 class="title">Country Name</h2>
+
+  <div class ='right-side-bar'>
+    <div class="details">
+      <div class="left-block">
+        <p> Fuel: titanium </p>
+        <p>Weight: 400</p>
+    </div>
+
+      <div class="right-block">
+        <p>Length: 10,000</p>
+        <p>Power: 20,000</p>
+      </div>
+    </div>
+
+    <h2>Comments</h2>
+    <div class="comment-details">
+
+    <span class="comment-body">
+      <p>03/11/2021: I'd like to visit this country</p>
+      <p>03/11/2021: I was there last summer</p>
+
+    </span>
+    </div>
+
+    <form id="form" action="">
+      <div class = 'error-msg'> </div>
+      <input id = 'name-input' type="text" placeholder="Your Name">
+      <textarea name=" message" id="msg-input" cols="30" rows="10" placeholder="Your insights"></textarea>
+      <button id="comment-btn" type="submit">Comment</button>
+    </form>
+  </div>
+</div>
+`;
+section.appendChild(windowContainer);
+
+/* Pop Up Button */
+const popWindow = document.querySelector('.comment-btn');
+
+popWindow.addEventListener('click', (e) => {
+  e.preventDefault();
+  windowContainer.style.display = 'flex';
+});
+
+const exitBtn = document.querySelector('.fa-x');
+exitBtn.addEventListener('click', () => {
+  windowContainer.style.display = 'none';
+});
+
+const form = document.querySelector('#form');
+const error = document.querySelector('.error-msg');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const nameInput = document.querySelector('#name-input').value;
+  const msgInput = document.querySelector('#msg-input').value;
+  if (nameInput === '' || msgInput === '') {
+    error.innerHTML = 'Please fill the fields';
+  }
+});
