28036312575b57a236eb161a09574e7f0588b25c
Aminu Rabiu <arabiu033@gmail.com>
2022-10-21 12:31:39 +0100

    Add the testing for items count

---
+++ src/modules/ItemsCount.test.js
@@ -0,0 +1,44 @@
+/** @jest-environment jsdom */
+/* eslint-disable global-require */
+
+test('test for three items added', () => {
+  document.body.innerHTML = `
+    <ul><ul>
+  `
+  const ItemsCount = require('./itemsCount.js');
+  const ul = document.querySelector('ul');
+  ul.innerHTML = `
+    <li>
+    <div class="img-holder"></div>
+    <section class="info"></>
+    <button type="button"></button>
+    </li>
+
+    <li>
+    <div class="img-holder"></div>
+    <section class="info"></>
+    <button type="button"></button>
+    </li>
+
+    <li>
+    <div class="img-holder"></div>
+    <section class="info"></>
+    <button type="button"></button>
+    </li>
+  `;
+
+  expect(ItemsCount.itemsCounter()).toBe(3);
+})
+
+test('test for zero items', () => {
+  document.body.innerHTML = `
+    <ul><ul>
+  `
+  const ItemsCount = require('./itemsCount.js');
+  const ul = document.querySelector('ul');
+  ul.innerHTML = `
+
+  `;
+
+  expect(ItemsCount.itemsCounter()).toBe(0);
+})
